4f4026b42fe189d11a346a237e9ead718830d037
miguelNPB <miguelnp@ucm.es>
2026-03-02 15:54:57 +0100

    Organizacion inicial y boot

---
+++ Assets/Scripts/Systems/GameSystem.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GameSystem : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static GameSystem Instance { get { return instance; } }
+    private static GameSystem instance = null;
+}
