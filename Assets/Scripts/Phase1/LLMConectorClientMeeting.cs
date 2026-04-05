a1f05011c101bd91ffcca02bb390cf5a1fdcacd7
Adelpozo04 <addelp01@ucm.es>
2026-04-05 02:09:35 +0200

    avances conversacion con cliente fase 1

---
+++ Assets/Scripts/Phase1/LLMConectorClientMeeting.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LLMConectorClientMeeting : LLMConector
+{
+    [SerializeField]
+    private UIClientMeetingManager _uiMeeting;
+
+    [Serializable]
+    private class MeetingResponse
+    {
+        public string answer;
+        public bool acceptTerms;
+    }
+
+    [Header("Search Messages")]
+    public TMP_InputField inputField;
+
+    private List<String> _historical = new List<string>();
+
+    /**
+     * Metodo encargado de recoger la respuesta del LLM y transmitirla a la clase que muestre el output de este
+     * @param success: muestra si ha podido obtenerse una respuesta del LLM
+     * @param answer: texto plano que ha sacado el LLM como output
+     */
+    public override void RecieveChatMessage(bool success, string answer)
+    {
+        Debug.Log("Respuesta cruda: " + answer);
+
+        if (success)
+        {
+            // deserializamos la respuesta
+            MeetingResponse jsonResponse = JsonUtility.FromJson<MeetingResponse>(answer);
+
+            _uiMeeting.EndPendingMessage(jsonResponse.answer);
+
+            if (_stepCounter < _config.getStepsChecks().Length)
+            {
+                securityStepsCheck(jsonResponse.answer);
+            }
+            else
+            {
+                Debug.Log("Respuesta final");
+                _historical.Add("Respuesta :" + jsonResponse.answer);
+                _stepCounter = 0;
+                _uiMeeting.ShowMessage();
+            }
+        }
+        else
+        {
+            Debug.LogError("Error en la llamada al LLM: " + answer);
+            _uiMeeting.EndPendingMessage("Error al contactar con el modelo.");
+        }
+
+    }
+
+    /**
+     * Metodo encargado de enviar un mensaje al LLM con todas las especificaciones obtenidas de ConfigLLMInfo
+     */
+    public override void SendChatMessage()
+    {
+        JsonSchema schema = new JsonSchema();
+
+        schema.properties.Add("answer", new PropertyInfo(JsonDataType.String));
+        schema.properties.Add("acceptTerms", new PropertyInfo(JsonDataType.Boolean));
+
+        string prompt = inputField.text;
+
+        string configLLM = _config.getContext()
+            + _config.getSafeguard();
+
+        configLLM = configLLM + "\n " + _config.getHistoricalConversation() + "\n Historico: \n";
+
+        foreach (String s in _historical)
+        {
+            configLLM = configLLM + s + "\n";
+        }
+
+        Debug.Log("PROMPT: " + prompt);
+        Debug.Log("CONTEXT: " + configLLM);
+
+        _historical.Add("Pregunta: " + prompt);
+
+        LLMAttorney_API.Instance.SendPrompt(API_TYPE.LLAMA, RecieveChatMessage, prompt, configLLM, schema,
+            _config.getTemperature(), _config.getRagUse(), (int)_config.getRagFileType());
+
+        inputField.text = "";
+
+        _uiMeeting.StartPendingMessage();
+    }
+
+    public override void securityStepsCheck(string prompt)
+    {
+        JsonSchema schema = new JsonSchema();
+
+        schema.properties.Add("answer", new PropertyInfo(JsonDataType.String));
+        schema.properties.Add("acceptTerms", new PropertyInfo(JsonDataType.Boolean));
+
+        Debug.Log("PROMPT de security checks: " + prompt);
+
+        string configLLM = "Teniendo el siguiente texto: \n" + prompt + "\n Y teniedo la siguiente directiva de seguridad" + _config.getSafeguardSteps() +
+            "\n Quiero que hagas lo siguiente: " + _config.getStepsChecks()[_stepCounter];
+
+        StartCoroutine(CoroutineSendPrompt(prompt, configLLM, schema));
+
+        inputField.text = "";
+
+        //_uiSearch.AddMessage(prompt);
+        _uiMeeting.StartPendingMessage();
+
+        _stepCounter++;
+    }
+
+    private IEnumerator CoroutineSendPrompt(string prompt, string configLLM, JsonSchema schema)
+    {
+
+        float timer = 0;
+
+        while (!LLMAttorney_API.Instance.SendPrompt(API_TYPE.LLAMA, RecieveChatMessage, prompt, configLLM, schema,
+            _config.getTemperature(), false))
+        {
+            timer += Time.deltaTime;
+
+            yield return null;
+        }
+
+
+
+    }
+}
