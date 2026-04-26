04dbf765df0a0ee598ded46a5b28d7f75d970ca5
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2026-04-27 01:22:47 +0200

    Arreglo cambio de escenas, prefab cambio de escena

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,46 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] 
+    private int sceneNumber;
+
+    void Awake()
+    {
+        Button but = GetComponent<Button>();
+        if (but != null)
+            but.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        switch (sceneNumber)
+        {
+            case 0:
+                SceneSystem.Instance.LoadMainMenu();
+                break;
+            case 1:
+                SceneSystem.Instance.LoadPhase1();
+                break;
+            case 2:
+                SceneSystem.Instance.LoadPhase2();
+                break;
+            case 3:
+                SceneSystem.Instance.LoadPhase3();
+                break;
+            case 4:
+                SceneSystem.Instance.LoadPhase4();
+                break;
+            case 5:
+                SceneSystem.Instance.LoadGameOver();
+                break;
+            default:
+                Debug.LogError("Scene number not recognized: " + sceneNumber);
+                break;
+        }
+    }
+
+
+}
