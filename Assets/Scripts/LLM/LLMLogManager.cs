000e80d394eca25b85eccf9a4a39727d8945f6aa
Adelpozo04 <addelp01@ucm.es>
2026-04-26 18:42:54 +0200

    sistema de almacenado de logs sin testeo

--- Assets/Scripts/LLM/LLMLogManager.cs
+++ Assets/Scripts/LLM/LLMLogManager.cs
@@ -1,16 +1,68 @@
+using System.IO;
+using Telemetry;
 using UnityEngine;
 
 public class LLMLogManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private LLMLogManager _instance = null;
+
+    public LLMLogManager Instance
     {
-        
+        get { return _instance; }
     }
 
-    // Update is called once per frame
-    void Update()
+    private string _path; 
+
+    private int _messageSent = 0;
+    private int _messageReceived = 0;
+
+
+    public void LogMessageSent(string message)
     {
-        
+        string log = $"{System.DateTime.Now} [{_messageSent}] {message}";
+
+        Debug.Log(log);
+        _messageSent++;
+    }
+
+    public void LogMessageReceived(string message)
+    {
+        string log = $"{System.DateTime.Now} [{_messageReceived}] {message}";
+
+        Debug.Log(log);
+        _messageReceived++;
+    }
+
+    public int getNumMessageSent()
+    {
+        return _messageSent;
+    }
+
+    public int getNumMessageReceived()
+    {
+        return _messageReceived;
+    }
+
+    private void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        string msg = $"{System.DateTime.Now} [{type}] {logString}\n";
+        System.IO.File.AppendAllText(_path, msg);
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+
+            _path = Application.persistentDataPath + "/game.log";
+            Application.logMessageReceived += HandleLog;
+
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 }
