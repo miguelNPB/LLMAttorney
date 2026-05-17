5263e76833d7debfde124cc319db92b2e0cbf21c
Javier Murillo González <96345143+LittleWallSt@users.noreply.github.com>
2026-05-17 20:35:13 +0200

    Update LLMConnectorClientIntroductionGenerator.cs

---
+++ Assets/Scripts/CaseGen/LLMConnectorClientIntroductionGenerator.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LLMConnectorClientIntroductionGenerator : LLMConnector
+{
+    [Serializable]
+    private class CaseIntroduction
+    {
+        public string introduction;
+    }
+
+    public event Action<string> onError;
+
+    protected override void createJsonSchemas()
+    {
+        _contextSchema = new JsonSchema();
+        _contextSchema.properties.Add("introduction", new PropertyInfo(JsonDataType.String));
+    }
+
+    public void SendPrompt(Action<string> responseCallback, string prompt)
+    {
+        sendPrompt(receiveResponse, prompt, 0);
+    }
+
+
+    private void receiveResponse(string answer)
+    {
+        CaseIntroduction jsonResponse = JsonUtility.FromJson<CaseIntroduction>(answer);
+        string filePath = System.IO.Path.Combine(Application.persistentDataPath, "CaseSummary.json");
+        File.WriteAllText(filePath, answer);
+    }
+
+    protected override string deseralizePromptFirstResponse(string serializedResponse)
+    {
+        CaseIntroduction jsonResponse = JsonUtility.FromJson<CaseIntroduction>(serializedResponse);
+        return serializedResponse;
+    }
+
+    protected override string deseralizePromptStepResponse(string serializedResponse)
+    {
+        CaseIntroduction jsonResponse = JsonUtility.FromJson<CaseIntroduction>(serializedResponse);
+        return serializedResponse;
+    }
+    private void Awake()
+    {
+        createJsonSchemas();
+    }
+
+
+}
