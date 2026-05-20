11bf151047ba89857f821c6a057afcceb26516a6
Adelpozo04 <addelp01@ucm.es>
2026-05-20 17:42:42 +0200

    conectores de la fase 1 comentados

--- Assets/Scripts/Phase1/LLMConnectors/LLMConnectorBudgetChecker.cs
+++ Assets/Scripts/Phase1/LLMConnectors/LLMConnectorBudgetChecker.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using Telemetry;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
+/// <summary>
+/// LLMConnector enfocado en la resvisión del presupuesto dado por el usuario, comprobando que no supera un umbral sobre el precio real y que 
+/// devuelve una media del presupuesto dado por el usuario.
+/// </summary>
 public class LLMConnectorBudgetChecker : LLMConnector
 {
     [Serializable]
@@ -11,13 +13,15 @@ public class LLMConnectorBudgetChecker : LLMConnector
         public bool budgetCoherent;
         public float budget;
     }
+
     private Action<bool, float> _responseCallback;
 
     /// <summary>
-    /// Metodo publico para activar el funcionamiento de este LLMConnector
+    /// Metodo publico para iniciar la llamada al LLM con los parametros y configuracion especificados
     /// </summary>
-    /// <param name="responseCallback"></param>
-    /// <param name="prompt"></param>
+    /// <param name="responseCallback">Metodo que debe llamarse una vez terminado el envio y recibida la contestación del LLM</param>
+    /// <param name="errorCallback">Metodo que debe llamarse si el envio del prompt al LLM es fallido debido a un problema del servidor</param>
+    /// <param name="prompt">Prompt escrito por el usuario que se desea enviar al LLM</param>
     public void SendPrompt(Action<bool, float> responseCallback, Action<string> errorCallback, string prompt)
     {
         _responseCallback = responseCallback;
@@ -27,7 +31,7 @@ public class LLMConnectorBudgetChecker : LLMConnector
     /// <summary>
     /// Metodo final para devolver la respuesta
     /// </summary>
-    /// <param name="finalSerializedResponse"></param>
+    /// <param name="finalSerializedResponse">Texto en formato json devuelto por el servidor que cuenta con los atributos rellenados por el LLM</param>
     private void recieveFinalResponse(string finalSerializedResponse)
     {
         BudgetCheckerResponse jsonResponse = JsonUtility.FromJson<BudgetCheckerResponse>(finalSerializedResponse);
@@ -35,6 +39,9 @@ public class LLMConnectorBudgetChecker : LLMConnector
         _responseCallback?.Invoke(jsonResponse.budgetCoherent, jsonResponse.budget);
     }
 
+    /// <summary>
+    /// Creacion de los esquemas especificos para este conector
+    /// </summary>
     protected override void createJsonSchemas()
     {
         _contextSchema = new JsonSchema();
