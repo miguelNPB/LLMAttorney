278a6861d837ce5f7e2165b9535b856c9ee39fa4
miguelNPB <miguelnp@ucm.es>
2026-05-20 20:23:55 +0200

    quitado includes que no permitian hacer build

--- LLMAttorneySimulator/Assets/Scripts/CaseGeneration/CaseGenerationManager.cs
+++ LLMAttorneySimulator/Assets/Scripts/CaseGeneration/CaseGenerationManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using TMPro;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UI;
 
--- LLMAttorneySimulator/Assets/Scripts/Phase2/DocumentGenerationManager.cs
+++ LLMAttorneySimulator/Assets/Scripts/Phase2/DocumentGenerationManager.cs
@@ -1,7 +1,4 @@
-using Newtonsoft.Json.Serialization;
 using System;
-using System.Collections;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 /// <summary>
